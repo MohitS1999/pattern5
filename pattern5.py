6bc1bbff23d0667f91d111d4e22297d0286dc4c2
Mohit singh <singhmohit9718@gmail.com>
2020-02-03 20:19:45 +0530

    PTYHON

---
+++ pattern5.py
@@ -0,0 +1,12 @@
+#5
+##             *****
+##             ****
+##             ***
+##             **      
+##             *     
+def pattern(n):
+    for i in range(n):
+        for j in range(n):
+             print('*',end='') if i+j<=n-1 else print(' ',end='')
+        print()
+pattern(5)
